c9edfdb28be4408a4a19b2982dac695c071b2c31
Kirill Dudnik <prog.me.2017@gmail.com>
2020-10-16 16:46:55 +0600

    The project was created! Version - 1.0 - configured the build for Android; - added static eyes for all skins; - added a skin - red cube; - added PHYSICAL movement of the player; - added physical walls without movement, like prefabs; -------------- Проект создан! Версия - 1.0 - настроил сборку под Андроид; - добавил статические глаза для всех скинов; - добавил скин - красный куб; - добавил ФИЗИЧЕСКОЕ движение игрока; - добавил физические стены без движения, как префабы;

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    [Header("Move Params")]
+    [SerializeField] private float _jumpForce = 2f;
+    private bool _isPlayerMove = false;
+    private sbyte _moveDirection = 0;
+    private float _wallPosX = 1.815f;
+    new Vector3 movementOffset;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        movementOffset = new Vector3(_jumpForce, 0f, 0f);
+    }
+
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        CheckPos();
+    }
+
+    private void Move()
+    {
+        if (!_isPlayerMove)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < _wallPosX)
+            {
+                _rb.isKinematic = false;
+                _isPlayerMove = true;
+                _moveDirection = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -_wallPosX)
+            {
+                _rb.isKinematic = false;
+                _isPlayerMove = true;
+                _moveDirection = -1;
+            }
+            else
+            {
+                _moveDirection = 0;
+            }
+            _rb.AddForce(new Vector3(_jumpForce * _moveDirection, 0f, 0f));
+        }
+    }
+
+    private void CheckPos()
+    {
+        if(transform.position.x > -_wallPosX || transform.position.x < _wallPosX)
+        {
+            _rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Wall")
+        {
+            Debug.Log("Collision");
+            _rb.isKinematic = true;
+            _isPlayerMove = false;
+            if(transform.position.x > 0)
+            {
+                transform.SetPositionAndRotation(new Vector3(_wallPosX, 0), Quaternion.identity);
+            }
+            else
+            {
+                transform.SetPositionAndRotation(new Vector3(-_wallPosX, 0), Quaternion.identity);
+            }
+        }
+    }
+}
